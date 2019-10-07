1f4f843b7a8a306d26268f987bc04e5d04f52f12
samarthhegdekalgar <samarthhegdekalgar@gmail.com>
2019-10-07 11:43:56 +0530

    Simple way to define list,set, and dictionary in python

---
+++ comma_trick.py
@@ -0,0 +1,12 @@
+name = ['samarth',
+        'salman',
+        'varun', ]
+second_name = {'Hegde',
+               'ahamed',
+               'pandey', }
+name_dict = {'first_name': name,
+             'second_name': second_name, }
+
+# Python will not throw any error if LIST,SET,DICTIONARY last elements are ended with comma
+# This trick will help us to check modified content in Git
+
